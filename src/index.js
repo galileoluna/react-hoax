bb87cc92790dcffa3411de34811a7eca115d71d1
galileoluna <galileoungs@gmail.com>
2020-07-05 12:13:36 -0300

    generate user singup testing

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { UserSignupPage } from './pages/UserSignupPage';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <UserSignupPage />,
   document.getElementById('root')
 );
 
